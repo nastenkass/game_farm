0d74814540375fb0e1400931a50344f4ee767490
nastenkass <pricekolfild@mail.ru>
2025-03-16 15:11:14 +0300

    fix 0.1 (BETA_VERSION_1)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-//Initializes the game in a local/offline environment
-//This assumes that data/processed-data.js has been generated, using the build.bat script
-//It also requires the data to be rebuilt everytime a change is made in the characters, places or story files.
 function init_local()
 {
 	initCharacters();
@@ -8,11 +5,7 @@ function init_local()
 	onInit();
 }
 
-//Initializes the game in a server environment
-//Doesn't rely on the data/processed-data.js file, and asynchronously loads the .json files instead
-//A server environment (or a local *AMP stack, node, python microserver...) is required for the $.getJSON calls to work
-//Except if your browser is particularly chill, in which case, maybe it works okay?
-//I don't know, mine isn't.
+
 function init_async()
 {
 	$.getJSON("data/characters.json", function(jsonCharacters) {
@@ -36,9 +29,9 @@ function init_async()
 
 function loadLevelData(level) {
     const script = document.createElement("script");
-    script.src = `data/processed-data-level${level}.js`; // Загружаем нужный файл уровня
+    script.src = `data/processed-data-level${level}.js`;
     script.onload = function () {
-        init_local(); // После загрузки данных инициализируем игру
+        init_local();
     };
     script.onerror = function () {
         console.error("Не удалось загрузить данные для уровня " + level);
@@ -48,41 +41,25 @@ function loadLevelData(level) {
 
 function getLevelFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('level') || 1; // По умолчанию уровень 1, если параметр не указан
+    return urlParams.get('level') || 1;
 }
 
 
 window.currentLevel = getLevelFromURL();
 loadLevelData(window.currentLevel);
 
-//Initialize the character data after the data has been loaded
 function initCharacters()
 {
 	for(var c in characters)
 	{
-		//Set a default approval level for all characters
 		characters[c].approval = 0;
 	}
 }
 
-//Called when the game is fully initialized
 function onInit()
 {
-	//Only one thing left to do: start the game on the first Scene \o/ Here we go~
 	displayScene("start");
 }
-
-//Are you running the game in a server environment?
-//If you don't, or don't know if you do:
-//- It's okay I still like you;
-//- You will need to double-click build.bat everytime you edit the characters, places or story files;
-//- You'll start the game by double-clicking index.html or game.html
-//- You don't need to touch anything below, you can close this file :)
-
-//If you do:
-//- Great, you can edit the characters, places and story files and see your changes immediately in the game;
-//- Do not start the game by double clicking the html files, use your server path instead;
-//- Comment init_local() and uncomment init_async(), and you're done :)
 
 init_local();
 //init_async();
